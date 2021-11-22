95e51d7a1ebcd42c394edd9cbb0874ebcf39150a
sand war <85954068+VBoschMatas@users.noreply.github.com>
2021-11-22 17:29:06 +0100

    Moving EBO and preparing fro review

--- Source/ModuleRender.cpp
+++ Source/ModuleRender.cpp
@@ -181,13 +181,13 @@ unsigned int ModuleRender::CreateVBO()
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	ebo = CreateEBO();
-
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (void*)0);
 	glEnableVertexAttribArray(0);
 
 	//glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 5, (void*)(3 * sizeof(float)));
 	//glEnableVertexAttribArray(1);
+
+	ebo = CreateEBO();
 
 	return vbo;
 }
